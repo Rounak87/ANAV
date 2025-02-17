6e4ea6fdd41c8bd6b982be8968b6c39a36abf937
Rounak Singh <156055860+Rounak87@users.noreply.github.com>
2025-02-17 22:30:00 +0530

    Update master.py

--- master.py
+++ master.py
@@ -1,9 +1,9 @@
 import os
 
-print("🚀 Starting Safe Spot Detection Process...")
+print("Starting ")
 
 os.system("python scripts/detect_boundary.py")
 os.system("python scripts/safe_spot_detection.py")
 os.system("python scripts/extract_safe_spots.py")
 
-print("✅ Process Completed. Check Outputs Folder.")
+print("Process Completed.")
